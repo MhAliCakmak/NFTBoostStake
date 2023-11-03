cd185fc327b3ddaaa8033cf2865b1d45430bf3db
mehmet5643 <202803055@ogr.cbu.edu.tr>
2023-11-03 12:13:45 +0300

    added stake.sol

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,12 +1,17 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.19;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
 contract BoostStakeToken is ERC20, Ownable, ERC20Permit {
-    constructor(address initialOwner)
+    uint256 public taxRate = 5; // 5%
+    mapping(address => bool) public whitelist;
+
+    constructor(
+        address initialOwner
+    )
         ERC20("Boost Stake Token", "BST")
         Ownable(initialOwner)
         ERC20Permit("Boost Stake Token")
@@ -16,5 +21,31 @@ contract BoostStakeToken is ERC20, Ownable, ERC20Permit {
 
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
+    }
+
+    function _transfer(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) internal override {
+        if (!whitelist[sender]) {
+            uint256 tax = (amount * taxRate) / 100;
+            super._transfer(sender, recipient, amount - tax);
+            super._transfer(sender, owner(), tax);
+        } else {
+            super._transfer(sender, recipient, amount);
+        }
+    }
+
+    function updateTax(uint _tax) external onlyOwner {
+        taxRate = _tax;
+    }
+
+    function addToWhitelist(address _address) external onlyOwner {
+        whitelist[_address] = true;
+    }
+
+    function removeFromWhitelist(address _address) external onlyOwner {
+        whitelist[_address] = false;
     }
 }
