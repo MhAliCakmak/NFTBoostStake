90ac1f1658e76bb645b7ce24022b030a38ab5717
mehmet5643 <202803055@ogr.cbu.edu.tr>
2023-11-06 23:21:50 +0300

    Add web3modal and axios dependencies, update ContractContext to connect to BSC Testnet

--- context/ContractContext.js
+++ context/ContractContext.js
@@ -1,8 +1,89 @@
-import React, { useState, useEffect } from 'react';
-import Web3Modal from 'web3modal';
-import { ethers } from 'ethers';
-import axios from 'axios';
-import { useRouter } from 'next/router';
+import React, { useState, useEffect } from "react";
+import Web3Modal from "web3modal";
+import { ethers } from "ethers";
+import axios from "axios";
+import { useRouter } from "next/router";
 import {
-  TokenAddress, StakeAddress, NFTAddress, NFTAbi, StakeAbi, TokenAbi,
-} from '../constants';
+  TokenAddress,
+  StakeAddress,
+  NFTAddress,
+  NFTAbi,
+  StakeAbi,
+  TokenAbi,
+} from "../constants";
+
+const ContractContext = React.createContext();
+export function ContractProvider({ children }) {
+  const router = useRouter();
+  const [currentAccount, setCurrentAccount] = useState("");
+
+  const checkIfWalletIsConnected = async () => {
+    const { ethereum } = window;
+
+    if (!ethereum) {
+      return;
+    }
+    try {
+      await ethereum.request({
+        method: "wallet_switchEthereumChain",
+        params: [{ chainId: "0x61" }],
+      });
+    } catch (switchError) {
+      if (switchError.code === 4902) {
+        await ethereum.request({
+          method: "wallet_addEthereumChain",
+          params: [
+            {
+              chainId: "0x61",
+              chainName: "BSC Testnet",
+              nativeCurrency: {
+                name: "BNB",
+                symbol: "BNB",
+                decimals: 18,
+              },
+              rpcUrls: ["https://data-seed-prebsc-1-s1.binance.org:8545/"],
+              blockExplorerUrls: ["https://testnet.bscscan.com/"],
+            },
+          ],
+        });
+      }
+    }
+
+    const accounts = await ethereum.request({ method: "eth_accounts" });
+
+    if (accounts.length !== 0) {
+      const account = accounts[0];
+
+      setCurrentAccount(account);
+    }
+  };
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, [currentAccount]);
+
+  const connectWallet = async () => {
+    const { ethereum } = window;
+
+    if (!ethereum) {
+      return;
+    }
+    await ethereum.request({
+      method: "wallet_switchEthereumChain",
+      params: [{ chainId: "0x61" }],
+    });
+    const accounts = await ethereum.request({ method: "eth_accounts" });
+    setCurrentAccount(accounts[0]);
+    window.location.reload();
+  };
+
+  return (
+    <ContractContext.Provider
+      value={{
+        currentAccount,
+        connectWallet,
+      }}
+    >
+      {children}
+    </ContractContext.Provider>
+  );
+}
--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,23 +1,28 @@
-import { ThemeProvider } from 'next-themes';
-import Script from 'next/script';
-import { Navbar, Footer } from '../components';
-import '../styles/globals.css';
+import { ThemeProvider } from "next-themes";
+import Script from "next/script";
+import { Navbar, Footer } from "../components";
+import { ContractProvider } from "../context/ContractContext";
+import { useState, useContext } from "react";
+import { useRouter } from "next/router";
+import "../styles/globals.css";
 
 function App({ Component, pageProps }) {
   return (
-    <ThemeProvider attribute="class">
-      <div className="dark:bg-nft-dark bg-white min-h-screen">
-        <Navbar />
-        <div className="pt-65">
-          <Component {...pageProps} />
+    <ContractProvider>
+      <ThemeProvider attribute="class">
+        <div className="dark:bg-nft-dark bg-white min-h-screen">
+          <Navbar />
+          <div className="pt-65">
+            <Component {...pageProps} />
+          </div>
+          <Footer />
         </div>
-        <Footer />
-      </div>
-      <Script
-        src="https://kit.fontawesome.com/fb64a6da9d.js"
-        crossOrigin="anonymous"
-      />
-    </ThemeProvider>
+        <Script
+          src="https://kit.fontawesome.com/fb64a6da9d.js"
+          crossOrigin="anonymous"
+        />
+      </ThemeProvider>
+    </ContractProvider>
   );
 }
 
